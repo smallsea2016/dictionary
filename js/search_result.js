df146d584e34285e40a7960209d73ea4073b3c87
smallsea2016 <1066328715@qq.com>
2017-05-24 10:10:10 +0800

    搜索高亮

--- js/search_result.js
+++ js/search_result.js
@@ -15,6 +15,7 @@ function clearValue(){
 }
 function search(){
     console.log('搜索');
+    highLight('.search_ipt');
     return  false
 }
 
@@ -34,7 +35,6 @@ $("#js-swiper-tab>li").on('touchstart mousedown',function(){
     tabSwiper._slideTo($(this).index());
 })
 
-
 /*伸缩内容*/
 function toggleContent(_this){
     var item = $(_this).parents('.dict_list').find('[data-role="toggle_item"]'),
@@ -47,3 +47,44 @@ function toggleContent(_this){
     // });
 }
 
+/*滚动加载*/
+function setScrollCss(){
+    var getDivHeight = function(){
+        var topHeights = document.querySelectorAll('[data-role="top-height"]');
+         var h = [],r;
+        for (var i = 0; i < topHeights.length; i++) {
+            h.push(topHeights[i].clientHeight);
+            r = eval(h.join('+'))
+        };
+        return r
+    }
+    $('[data-role="scroll-height"]').css({
+        height: ($(window).height() - getDivHeight())+'px',
+        overflowY: 'auto'
+    });
+}
+setScrollCss();
+
+var globalTime = null;
+function scrollFn(_this){
+    var nScrollHight = _this.scrollHeight;
+    var nScrollTop = _this.scrollTop;
+    var nDivHight = _this.clientHeight;
+   if(nScrollTop + nDivHight >= nScrollHight){
+        clearTimeout(globalTime);
+        globalTime = setTimeout(function(){ //这里用定时器是为了节流
+            // loadData();  
+            console.log('加载更多');
+        },100)   
+    } 
+}
+
+/*搜索结果高亮*/
+function highLight(el) {
+    var searchTerm = $(el).val();
+    // console.log("searchTerm:"+searchTerm);
+    $('body').removeHighlight();
+    if ( searchTerm ) {
+        $('body').highlight( searchTerm );
+    }
+}
